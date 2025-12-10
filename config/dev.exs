b324c0a4db641b78da180a638de87c0515a33a38
Wonseok Yang <wsyang@digicaps.com>
2025-12-10 17:24:22 +0900

    magic link 추가

--- config/dev.exs
+++ config/dev.exs
@@ -85,3 +85,5 @@ config :phoenix_live_view,
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
+
+config :ash_authentication, debug_authentications_failures?: true
