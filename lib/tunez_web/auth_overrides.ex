773f2907236ead26f89073b3f924bfef956ffe63
Wonseok Yang <wsyang@digicaps.com>
2025-12-10 17:00:52 +0900

    로그인 화면 디자인 수정

--- lib/tunez_web/auth_overrides.ex
+++ lib/tunez_web/auth_overrides.ex
@@ -1,13 +1,15 @@
 defmodule TunezWeb.AuthOverrides do
   use AshAuthentication.Phoenix.Overrides
-
   alias AshAuthentication.Phoenix.Components
 
   override Components.Banner do
-    set :text, "🎵"
+    set(:image_url, nil)
+    set(:dark_image_url, nil)
+    set(:text_class, "text-8xl text-accent-400")
+    set(:text, "🎵")
   end
 
   override Components.Password.Input do
-    set :submit_class, "bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+    set(:submit_class, "bg-primary-600 text-white my-4 py-3 px-5 text-sm")
   end
 end
