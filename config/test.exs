50f8884a94589168b57341f6d8755bb3774e34ba
Wonseok Yang <wsyang@digicaps.com>
2025-12-10 11:08:12 +0900

    mix igniter.install ash_authentication

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,6 @@
 import Config
+config :tunez, token_signing_secret: "9xLI2XCMyj8yhedtt1UC+6xLnz/N8yU9"
+config :bcrypt_elixir, log_rounds: 1
 config :ash, policies: [show_policy_breakdowns?: true], disable_async?: true
 
 # Configure your database
