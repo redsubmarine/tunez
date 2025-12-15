5f8d6882489661aefa1663f995471c8c9d0645c2
Wonseok Yang <wsyang@digicaps.com>
2025-12-15 14:51:45 +0900

    album - tracks 관계 설정

--- lib/tunez/music/track.ex
+++ lib/tunez/music/track.ex
@@ -4,5 +4,35 @@ defmodule Tunez.Music.Track do
   postgres do
     table "tracks"
     repo Tunez.Repo
+
+    references do
+      reference :album, index?: true, on_delete: :delete
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :order, :integer do
+      allow_nil? false
+    end
+
+    attribute :name, :string do
+      allow_nil? false
+    end
+
+    attribute :duration_seconds, :integer do
+      allow_nil? false
+      constraints min: 1
+    end
+
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
+  end
+
+  relationships do
+    belongs_to :album, Tunez.Music.Album do
+      allow_nil? false
+    end
   end
 end
