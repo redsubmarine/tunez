3120b73d74edf758f4619f18e31d7b659f2fd33a
Wonseok Yang <wsyang@digicaps.com>
2025-12-16 11:43:25 +0900

    unfollow 로직 처리

--- test/tunez/accounts/artist_follower_test.exs
+++ test/tunez/accounts/artist_follower_test.exs
@@ -20,26 +20,25 @@ defmodule Tunez.Accounts.ArtistFollowerTest do
   end
 
   describe "Tunez.Music.unfollow_artist/2" do
-    @tag skip: "can be enabled during chapter 9"
     test "deletes a specific ArtistFollower record" do
-      # # Create some extra records so we can assert that only the correct link is deleted
-      # [artist_one, artist_two] = generate_many(artist(), 2)
-      # [user_one, user_two] = generate_many(user(), 2)
+      # Create some extra records so we can assert that only the correct link is deleted
+      [artist_one, artist_two] = generate_many(artist(), 2)
+      [user_one, user_two] = generate_many(user(), 2)
 
-      # Music.follow_artist!(artist_one, actor: user_one)
-      # Music.follow_artist!(artist_one, actor: user_two)
+      Music.follow_artist!(artist_one, actor: user_one)
+      Music.follow_artist!(artist_one, actor: user_two)
 
-      # Music.follow_artist!(artist_two, actor: user_one)
-      # Music.follow_artist!(artist_two, actor: user_two)
+      Music.follow_artist!(artist_two, actor: user_one)
+      Music.follow_artist!(artist_two, actor: user_two)
 
-      # assert Music.unfollow_artist!(artist_two, actor: user_one)
+      assert Music.unfollow_artist!(artist_two, actor: user_one)
 
-      # followers = Ash.load!(artist_two, :followers, authorize?: false).followers
-      # assert length(followers) == 1
-      # assert hd(followers).id == user_two.id
+      followers = Ash.load!(artist_two, :followers, authorize?: false).followers
+      assert length(followers) == 1
+      assert hd(followers).id == user_two.id
 
-      # followers = Ash.load!(artist_one, :followers, authorize?: false).followers
-      # assert length(followers) == 2
+      followers = Ash.load!(artist_one, :followers, authorize?: false).followers
+      assert length(followers) == 2
     end
   end
 
